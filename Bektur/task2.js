5713aa945e84ed26b32d85e90b1a4df52687a559
Bektur Batyrbekov <batyrbekovbektur0@gmail.com>
2024-01-17 14:13:53 +0600

    twoSum

--- Bektur/task2.js
+++ Bektur/task2.js
@@ -1,4 +1,4 @@
-const twoSun = function (nums, target) {
+const twoSum = function (nums, target) {
     let total = [];
     for(let i = 0; i < nums.length;i++){
         for (let j = i + 1; j < nums.length ;j++){
@@ -10,4 +10,4 @@ const twoSun = function (nums, target) {
     }
     return total
 };
-console.log(twoSun([2,7,11,15],9));
+console.log(twoSum([2,7,11,15],9));
