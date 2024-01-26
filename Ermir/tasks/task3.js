4b2a4a005fed98fb1ca7d2d7fe50c10785f1f66e
Asanbeko <ermirasanbekov63@gmail.com>
2024-01-26 13:53:01 +0600

    task.3

---
+++ Ermir/tasks/task3.js
@@ -0,0 +1,9 @@
+function checkAge (age) {
+    if (age >= 18) {
+        console.log("Совершеннолетний")
+    } else if (age < 18) {
+        console.log("Несовершеннолетний")
+    }
+}
+
+checkAge(16)
