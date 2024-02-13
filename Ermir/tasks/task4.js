ff9c4c865a87494ed9b155f927dd7e525914a8b9
Asanbeko <ermirasanbekov63@gmail.com>
2024-02-13 15:46:11 +0600

    task.5

---
+++ Ermir/tasks/task4.js
@@ -0,0 +1,31 @@
+let Host = {
+    guestList: [],
+
+    setGuestList: function(names) {
+        let rejectedNames = [];
+
+        console.log("Список гостей: " + names.length + " имен.");
+
+        for (let i = 0; i < names.length; i++) {
+            if (Math.random() > 0.2) {
+                Host.guestList.push(names[i]);
+            } else {
+                rejectedNames.push(names[i]);
+            }
+        }
+
+        console.log("После принятия решения хозяином  приглашено  " + Host.guestList.length + " человек.");
+
+        if (rejectedNames.length > 0) {
+            console.log("Хозяин не пригласил : " + rejectedNames.join(', ') + ".");
+        }
+
+        console.log("Хозяин пригласил: " + Host.guestList.join(', ') + ".");
+    }
+};
+
+let initialList = ['Ивон', 'Петр', 'Алексей', 'Мария', 'Селена', 'Наташа', 'Ольга', 'Маша'];
+Host.setGuestList(initialList);
+console.log(Host.guestList)
+
+
